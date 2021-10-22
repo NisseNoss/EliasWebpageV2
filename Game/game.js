b685cfa9d0e097faa87885143b040f2f5abe7784
NonStop-png <e-elivaa1@asvg.no>
2021-10-22 22:30:48 +0200

    suffer i did

--- Game/game.js
+++ Game/game.js
@@ -2,25 +2,73 @@
 //TODO Gjør vegger i grid
 class GridSystem { //TODO fortsette
     //TODO kommentere
-    constructor(matrix) {
+    constructor(matrix, pacmanX, pacmanY) {
         this.matrix = matrix;
-        this.uiContext = this.#getContext(900, 900, "#000"); //hei
-        this.outlineContext = this.#getContext(0, 0, "#000");
-        this.topContext = this.#getContext(0, 0, "#000", true);
+        this.uiContext = this.#makeCanvas(900, 900, "#000"); //hei
+        this.outlineContext = this.#makeCanvas(0, 0, "#000");
+        this.topContext = this.#makeCanvas(0, 0, "#000", true);
         this.cellSize = 24;
         this.padding = 1;
+        this.pacman = {x: pacmanX, y: pacmanY, color: "orange"}
+        this.matrix [pacmanY][pacmanX] = 3;
+
+        document.addEventListener("keydown", this.#movePacman)
     }
 
-    #getCenter (w, h) { // Sentrerer tingen
+    #isValidMove(x, y) {
+        if (this.matrix[this.pacman.y + y][this.pacman.x + x] === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    #updateMatrix(y, x, val) {
+        this.matrix[y][x] = val;
+    }
+
+    #movePacman = ({keyCode}) => {
+        if (keyCode === 65) { // Venstre
+            if (this.#isValidMove(-1, 0)) {
+                this.#updateMatrix(this.pacman.y, this.pacman.x, 0)
+                this.#updateMatrix(this.pacman.y, this.pacman.x - 1, 3)
+                this.pacman.x--;
+                this.loadPosition();
+            }
+        } else if (keyCode === 68) { // Høyre
+            if (this.#isValidMove(1, 0)) {
+                this.#updateMatrix(this.pacman.y, this.pacman.x, 0)
+                this.#updateMatrix(this.pacman.y, this.pacman.x + 1, 3)
+                this.pacman.x++;
+                this.loadPosition();
+            }
+        } else if (keyCode === 87) { // Opp
+            if (this.#isValidMove(0, -1)) {
+                this.#updateMatrix(this.pacman.y, this.pacman.x, 0)
+                this.#updateMatrix(this.pacman.y - 1, this.pacman.x, 3)
+                this.pacman.y--;
+                this.loadPosition();
+            }
+        } else if (keyCode === 83) { // Ned
+            if (this.#isValidMove(0, 1)) {
+                this.#updateMatrix(this.pacman.y, this.pacman.x, 0)
+                this.#updateMatrix(this.pacman.y + 1, this.pacman.x, 3)
+                this.pacman.y++;
+                this.loadPosition();
+            }
+        }
+    }
+
+    #getCenter(w, h) { // Sentrerer tingen
         return {
             x: window.innerWidth / 2 - w / 2 + "px",
             y: window.innerHeight / 2 - h / 2 + "px"
         };
     }
 
-    #getContext(w, h, color = "#111", isTransparent = false) {
+    #makeCanvas(w, h, color = "#111", isTransparent = false) {
         this.canvas = document.createElement("canvas");
-        this.context = this.canvas.getContext("2d");
+        this.context = this.#getContext()
+        //this.context = this.canvas.getContext("2d");
         this.width = this.canvas.width = w;
         this.height = this.canvas.height = h;
         this.canvas.style.position = "absolute";
@@ -35,6 +83,11 @@ class GridSystem { //TODO fortsette
 
         return this.context;
     }
+    
+    #getContext() {
+        this.context = this.canvas.getContext("2d");
+        return this.context;
+    }
 
     render() {
         const w = (this.cellSize + this.padding) * this.matrix[0].length - (this.padding)
@@ -43,6 +96,9 @@ class GridSystem { //TODO fortsette
         this.outlineContext.canvas.width = w;
         this.outlineContext.canvas.height = h;
 
+        this.topContext.canvas.width = w;
+        this.topContext.canvas.height = h;
+
         const center = this.#getCenter(w, h);
         this.outlineContext.canvas.style.marginTop = center.y;
         this.outlineContext.canvas.style.marginLeft = center.x;
@@ -50,21 +106,57 @@ class GridSystem { //TODO fortsette
         this.topContext.canvas.style.marginLeft = center.x;
         this.topContext.canvas.style.marginTop = center.y;
 
-        for (let row = 0; row < this.matrix.length; row ++ ) {
-            for (let col = 0; col < this.matrix[row].length; col ++) {
-                this.outlineContext.fillStyle = this.matrix[row][col] > 0 ? "#4488FF" : "#000"; // kan legge til flere med ||
+        for (let row = 0; row < this.matrix.length; row++) {
+            for (let col = 0; col < this.matrix[row].length; col++) {
+                const cellVal = this.matrix[row][col];
+                let color = "#111";
+                let pacColor = this.pacman.color;
+
+                if (cellVal === 1) {
+                    color = "#4488FF";
+                } else if (cellVal === 2) {
+                    color = "#FFCBFF";
+                }  /*else if (cellVal === 3) {
+                    color = this.pacman.color;
+                }*/
+                this.outlineContext.fillStyle = color;
                 this.outlineContext.fillRect(col * (this.cellSize + this.padding),
-                row * (this.cellSize + this.padding),
-                this.cellSize, this.cellSize);
-                }
+                    row * (this.cellSize + this.padding),
+                    this.cellSize, this.cellSize);
             }
+        }
         this.uiContext.font = "20px Courier";
         this.uiContext.fillStyle = "#fff";
         this.uiContext.fillText("Your mother fucker", 20, 30);
+    }
+
+    loadPosition() {
+        const w = (this.cellSize + this.padding) * this.matrix[0].length - (this.padding);
+        const h = (this.cellSize + this.padding) * this.matrix.length - (this.padding);
+
+        this.topContext.canvas.width = w;
+        this.topContext.canvas.height = h;
+
+        const center = this.#getCenter(w, h);
+        this.topContext.canvas.style.marginLeft = center.x;
+        this.topContext.canvas.style.marginTop = center.y;
+
+        for (let row = 0; row < this.matrix.length; row++) {
+            for (let col = 0; col < this.matrix[row].length; col++) {
+                const cellVal = this.matrix[row][col];
+
+                if (cellVal === 3) {
+                    let color = this.pacman.color
+                    this.topContext.fillStyle = color;
+                    this.topContext.fillRect(col * (this.cellSize + this.padding),
+                        row * (this.cellSize + this.padding),
+                        this.cellSize, this.cellSize);
+                }
+            }
 
         }
     }
-
+}
 const gridMatrix = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -78,7 +170,7 @@ const gridMatrix = [
     [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
@@ -99,5 +191,6 @@ const gridMatrix = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
-const gridSystem = new GridSystem(gridMatrix);
+const gridSystem = new GridSystem(gridMatrix,14, 23);
 gridSystem.render();
+gridSystem.loadPosition();
